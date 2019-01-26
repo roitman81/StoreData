62f71cdfe8f6f9c6d58473f3604217431454a5fd
Ilya Roitman <roitman81@gmail.com>
2019-01-26 23:42:40 +0200

    working

--- src/main/java/com/example/TestStore/web/AndroidController.java
+++ src/main/java/com/example/TestStore/web/AndroidController.java
@@ -1,10 +1,14 @@
 package com.example.TestStore.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,24 +28,41 @@ public class AndroidController {
 		List<Category> reshima = repository.findAll(sort);
 		return reshima;
 	}
-
-	@RequestMapping("categories/delete/{id}")
-	public int deleteCategory(@PathVariable("id") int id) {
+	
+	
+	
+	
+	@RequestMapping("deleteCat/{id}")
+	public HttpStatus deleteCategory(@PathVariable("id") int id) {
 		repository.deleteById(id);
-		return id;
+		System.out.println("delete category id: " + id);
+		return HttpStatus.ACCEPTED;
 
 	}
 
-	@RequestMapping("addCategory")
-	public Category addCategory(@RequestParam("Category") Category category) {
+	
+	
+
+	
+	@RequestMapping(value = "addCategory")
+	
+	public Category addCategory(@RequestBody Category category) {
 		repository.save(category);
 		return category;
 
 	}
 	
-	@RequestMapping("update")
-	public Category updateCategory(@RequestParam("Category") Category category) {
-		repository.save(category);
+	@RequestMapping("updateCat")
+	public Category updateCategory(@RequestBody Category category) {
+		Optional<Category> optCat = repository.findById(category.getId());
+		Category cat = new Category();
+		if (optCat.isPresent()) {
+			cat = optCat.get();
+		}
+		
+		cat.setName(category.getName());
+		
+		repository.save(cat);
 		return category;
 
 	}
